cdc95f0d80826e3b6700fbfc93a5384b4b40ce21
Majid.mhdp <majid.mhdp@gmail.com>
2024-05-18 10:01:58 +0800

    application layer

---
+++ TravelManagement.Application/Services/IWeatherService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Application.DTO.External;
+
+namespace TravelManagement.Application.Services
+{
+	public interface IWeatherService
+	{
+		Task<WeatherDto> GetWeatherAsync(Domain.ValueObjects.TravelerCheckListDestination localization);
+	}
+}
