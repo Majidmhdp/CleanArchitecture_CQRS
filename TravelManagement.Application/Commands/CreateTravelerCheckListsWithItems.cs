1511c76a2e0a9adaaf5d3f06fc0f5b1ee24f07ed
Majid.mhdp <majid.mhdp@gmail.com>
2024-05-18 10:17:54 +0800

    commands

---
+++ TravelManagement.Application/Commands/CreateTravelerCheckListsWithItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Domain.Consts;
+using TravelManagement.Domain.ValueObjects;
+using TravelManagement.Shared.Abstractions.Commands;
+
+namespace TravelManagement.Application.Commands
+{
+	public record CreateTravelerCheckListsWithItems(Guid Id, string Name, ushort Days, Gender Gender,
+		DestinationWriteModel Destination) : ICommand;
+
+	public record DestinationWriteModel(string City, string Country);
+
+}
